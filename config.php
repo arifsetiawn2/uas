c294f09b2c574501d6b6d68d228c61594b08fefe
arifsetiawn2 <114553528+arifsetiawn2@users.noreply.github.com>
2023-01-20 19:52:59 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,7 @@
+<!-- Reprogramed By Arif Setiawan -->
+<?php
+	$conn = new mysqli("localhost","root","","cart_system");
+	if($conn->connect_error){
+		die("Connection Failed!".$conn->connect_error);
+	}
+?>
